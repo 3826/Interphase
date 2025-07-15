c2266bcc195a0ad156f0543e0c959bd782e3f6d8
3826 <3826.code@gmail.com>
2025-07-15 11:29:27 +0200

    fixed phaser import

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // JS 1
-// import Phaser from 'phaser';
+import Phaser from 'phaser';
 import BootScene from './src/scenes/BootScene.js';
 import TitleScene from './src/scenes/TitleScene.js';
 import GameScene from './src/scenes/GameScene.js';
 import ResultScene from './src/scenes/ResultScene.js';
-// const config = {
+
+const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   scene: [BootScene, TitleScene, GameScene, ResultScene]
 };
+
 const game = new Phaser.Game(config);
-// export default game;
